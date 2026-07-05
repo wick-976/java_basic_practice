a188dc81b867c0c5c436326d96e2150e7ce4d9c6
wick-976 <cbtharan976@gmail.com>
2026-07-05 20:12:18 +0530

    Summation of natural numbers

---
+++ Sum_of_numbers.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Sum_of_numbers {
+    public static void  main(String [] args)
+    {
+     Scanner sc=new Scanner(System.in);
+     
+     int num1;
+     num1=sc.nextInt();
+
+     System.out.println(num1*(num1+1)/2);
+
+
+    }
+}
