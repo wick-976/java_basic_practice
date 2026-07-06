eaa22b8e5bd48bdffc5a08e8f42d945061836c34
wick-976 <cbtharan976@gmail.com>
2026-07-06 16:30:37 +0530

    Findin Numbers

--- finding_number.java
+++ finding_number.java
@@ -1,26 +1,37 @@
 import java.util.Scanner;
 public class finding_number {
+    public static  boolean find_number(int array[],int num1,int targert)
+{
+       
+        for(int i=0;i<num1;i++)
+        {
+            if(targert==array[i])
+            {
+                return true;
+            }
+        }
+       
+        return false;
+    
+}
     public static void main(String [] args)
     {
         int num1;
         Scanner sc=new Scanner(System.in);
         num1=sc.nextInt();
         int array[]=new int [num1];
-
-        for(int i=0;i<num1;i++)
+         for(int i=0;i<num1;i++)
         {
                array[i]=sc.nextInt();
         }
 
-        for(int i=0;i<num1;i++)
-        {
-            if(num1==array[i])
-            {
-                System.out.println("The given number is present inside the array");
-                return;
-            }
+        boolean result = find_number(array, num1,9);
+        boolean Result=  find_number(array, num1,7);
+        if (result==true) {
+            System.out.println("The given number is present inside the array");
+        } else {
+            System.out.println("The given number is not present inside the array");
         }
-        System.out.println("THe given number is not present inside the array");
 
     }
 }
